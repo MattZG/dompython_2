234854c3db6ff2e8ae07e572319f84622a113a96
MattZG <matias.zamorano09@gmail.com>
2023-09-28 19:36:48 -0300

    update

---
+++ Basic/Minu_Mayu.py
@@ -0,0 +1,7 @@
+texto = "El Nico es un peepoClown"
+
+print(texto.upper())
+print(texto.lower())
+print(texto.capitalize())
+print(texto.swapcase())
+print(texto.title())
---
+++ Basic/UUID.py
@@ -0,0 +1,7 @@
+import uuid
+
+codigo_uuid = uuid.uuid4()
+print(codigo_uuid, type(codigo_uuid))
+
+codigo_str = str(codigo_uuid)
+print(codigo_str, type(codigo_str))
